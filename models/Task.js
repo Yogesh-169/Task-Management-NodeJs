26c3e392166937462139594be62d1d107b033e01
Yogesh-169 <yogeshahir0000@gmail.com>
2025-02-02 18:16:59 +0530

    assign the task to the users

---
+++ models/Task.js
@@ -0,0 +1,14 @@
+// models/Task.js
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  dueDate: { type: Date },
+  status: { type: String, enum: ['pending', 'in progress', 'completed'], default: 'pending' }
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
