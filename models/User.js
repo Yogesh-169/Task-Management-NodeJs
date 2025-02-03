c8615d58951cd60a1f754f32b37208b6384b14bf
Yogesh-169 <yogeshahir0000@gmail.com>
2025-02-03 12:43:30 +0530

    default admin

--- models/User.js
+++ models/User.js
@@ -1,22 +1,35 @@
-// models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' } // New field for user roles
+  role: { type: String, enum: ['user', 'admin'], default: 'user' } // Role field
 }, { timestamps: true });
 
 // Hash password before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-  next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    // Check if there is any admin user in the database
+    const User = mongoose.model('User', userSchema);
+    const adminExists = await User.findOne({ role: 'admin' });
+
+    // If no admin exists, make this user the admin
+    if (!adminExists) {
+      this.role = 'admin';
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
